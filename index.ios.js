751d2f0f11b51f15de84808dccef0b8d0576dd41
NieChenghai <chenghai.nie@sap.com>
2016-12-09 17:09:17 +0800

    sample

--- index.ios.js
+++ index.ios.js
@@ -9,13 +9,106 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  Image,
+  View,
+  ScrollView 
+
 } from 'react-native';
 
-export default class fourmatrix extends Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+        <Text>{display}</Text>
+    );
+  }
+}
+
+class LotsOfStyles extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+      </View>
+    );
+  }
+}
+
+class FixedDimensionsBasics extends Component {
+  render() {
+    return (
+      <View>
+        <Text>FixedDimensionsBasics</Text>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+
+class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      <View style={{width: 400, height: 200, flexDirection: 'column'}}>
+        <Text>Flex</Text>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}} >
+          <View style={{width: 50, height: 50, flex: 1, backgroundColor: 'red'}} />
+          <View style={{width: 50, height: 50, flex: 1, backgroundColor: 'yellow'}} />
+          <View style={{width: 50, height: 50, flex: 1, backgroundColor: 'blue'}} />
+          <View style={{width: 50, height: 50, flex: 2, backgroundColor: 'pink'}} />
+        </View>
+      </View>
+    );
+  }
+};
+
+
+export default class AwesomeProject extends Component {
+  render() {
+
+    let name = "hello world!";
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
+    return (
+      <ScrollView>
+        <FlexDimensionsBasics />
+        <FixedDimensionsBasics />
+        <LotsOfStyles />
+        <Blink text={"Blink......"} />
+        <Blink text={"Blink......@@@@@......."} />
+        <Greeting name={name} />
+        <Greeting name={"test2"} />
+        <Greeting name={"test3"} />
+        <Greeting name={"test4"} />
+
+        <Image source={pic} style={{width: 193, height: 110}}/>
+
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -26,7 +119,7 @@ export default class fourmatrix extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -48,6 +141,14 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
 });
 
-AppRegistry.registerComponent('fourmatrix', () => fourmatrix);
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
